30fc6b8b51cd5c7f1f06bb793387f2ec26323e84
Mariam Tandashvili <73125781+mari839@users.noreply.github.com>
2023-08-20 17:17:51 +0400

    Update Program.cs

--- BooksManagementSystem/Program.cs
+++ BooksManagementSystem/Program.cs
@@ -1,13 +1,16 @@
 using BooksManagementSystem.DbContexts;
+using BooksManagementSystem.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
 // Add services to the container.
 builder.Services.AddDbContext<BookDbContext>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<IBookRepository, BookRepository>();
 
 var app = builder.Build();
 
