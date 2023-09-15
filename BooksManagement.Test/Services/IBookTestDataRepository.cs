1b2ddc2153acc8d1e381ed55cc9e0aae5f8a2ecf
mariami <mariam.tandashvili.1@btu.edu.ge>
2023-09-15 16:47:47 +0400

    added test project

--- BooksManagement.Test/Services/IBookTestDataRepository.cs
+++ BooksManagement.Test/Services/IBookTestDataRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BooksManagementSystem.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,10 @@ using System.Threading.Tasks;
 
 namespace BooksManagement.Test.Services
 {
-    internal class IBookTestDataRepository
+    public interface IBookTestDataRepository
     {
+        Book getBookByIdTest(int id);
+        List<Book?> getBooksListTest();
+        List<Book> searchBookByQuery(string query);
     }
 }
